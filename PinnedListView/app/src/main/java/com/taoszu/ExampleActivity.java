0ad9939a4e424f690e67d97afcdfde62771a4985
taoszu <taoszu@qq.com>
2017-03-09 14:42:56 +0800

    [项目新建] 1. 构建项目完成

---
+++ PinnedListView/app/src/main/java/com/taoszu/ExampleActivity.java
@@ -0,0 +1,14 @@
+package com.taoszu;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * author:by lin.ht on 2017/3/9.
+ * email: 645153817@qq.com
+ * version: 1.0
+ */
+
+public class ExampleActivity extends AppCompatActivity {
+
+
+}
